0e88a0981b73c9108725f6123bd6858bf8fa509e
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-04 23:37:42 +0100

    Add Validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, uniqueness: true
+    validates :username, presence: true
     validates_format_of :email, with: /@/
     validates :password_digest, presence: true
 end
