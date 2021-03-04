1140358edd315ae0dd242532b002404e13fc646d
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-04 23:41:32 +0100

    Move JsonWebToken Class into Token Controller

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,7 +1,22 @@
+class JsonWebToken
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+  
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    decoded = JWT.decode(token, SECRET_KEY).first
+    HashWithIndifferentAccess.new decoded
+  end
+
+end
+
 class Api::V1::TokensController < ApplicationController
   def create
-      @user = User.find_by_email(user_params[:email])
-      if @user&.authenticate(user_params[:password])
+      @user = User.find_by_email(params[:email])
+      if @user &.authenticate(params[:password])
         render json: {
           token: JsonWebToken.encode(user_id: @user.id),
           email: @user.email
@@ -10,10 +25,4 @@ class Api::V1::TokensController < ApplicationController
         head :unauthorized
       end
     end
-
-    private
-    def user_params
-      params.require(:user).permit(:email, :password)
-    end
-
 end
