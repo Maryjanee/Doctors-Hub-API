b2eaa6cffc1965e076b091577df00f320f0d8d67
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-04 16:59:04 +0100

    Update Routes for users and Tokens

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/logged_in', to: 'sessions#is_logged_in?'
-  
-  resources :users, only: [:index,:create, :show]
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users
+      resources :tokens
+    end
+  end
 end
