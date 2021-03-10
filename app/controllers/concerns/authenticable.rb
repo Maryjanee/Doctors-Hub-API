3c732419dad847e5e86e39e6bde62b202cd687fa
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-10 13:19:36 +0100

    add helper methods to authenticable module

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -5,7 +5,23 @@ module Authenticable
         header = request.headers['Authorization']
         return nil if header.nil?
         decoded = JsonWebToken.decode(header)
-        @current_user = User.find(decoded[:user_id]) rescue
+        @current_user = User.find(decoded[:user_id])
+     rescue
         ActiveRecord::RecordNotFound
+    end
+
+    def logged_in
+        if !current_user.nil?
+            render json: "You are not logged in, please do so!"
+        end
+
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
+    def check_owner
+        head :forbidden unless @user.id == current_user&.id
     end
 end
