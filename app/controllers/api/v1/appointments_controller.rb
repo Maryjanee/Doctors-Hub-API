1e557b9ad4f0f1eb89ae433c377c6c922190a21a
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-12 15:17:38 +0100

    delete index action from appointments

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -2,18 +2,11 @@ class Api::V1::AppointmentsController < ApplicationController
     before_action :current_user
 
 
-    def index
-        @appointments = Appointment.where(user_id: @current_user.id)
-        if @appointments
-          render json: @appointments
-        else
-          render json: 'No appointments yet'
-        end
-      end
-    
+  
       def create
         appointment = Appointment.new(appointment_params)
         appointment.user_id = @current_user.id
+        appointment.username = @current_user.username
     
         if appointment.save
           render json: appointment, status: :created
@@ -21,6 +14,15 @@ class Api::V1::AppointmentsController < ApplicationController
           render json: { errors: appointment.errors.full_messages }, status: 500
         end
       end
+
+      def show
+        @appointments = Appointment.where(user_id: @current_user.id)
+        if @appointments
+          render json: @appointments
+        else
+          render json: 'No appointments yet'
+        end
+    end
     
       def destroy
         appointment = Appointment.find_by(id: params[:id])
@@ -31,6 +33,8 @@ class Api::V1::AppointmentsController < ApplicationController
       private
     
       def appointment_params
-        params.require(:appointment).permit(:user_id, :doctor_id, :date, :city)
+        params.require(:appointment).permit(:doctor_id, :doctor_name, :date, :city)
       end
+
+      
 end
