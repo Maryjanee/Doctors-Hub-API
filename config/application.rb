d0748e59c11cbe52da57045ba7c783d6167b097c
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-20 14:55:00 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module DoctorsHubApi
     config.eager_load_paths << Rails.root.join('lib')
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    config.autoloader = :classic
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
